dfb3609a68d7ed9d933cb642fe4f6a5b6453063e
Surya Sairama Sampath <suryasairamasampath@Suryas-MacBook-Air.local>
2026-02-03 13:39:23 -0500

    Add gh-pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your GitHub repo name
-})
+  base: "/portfolio/", // repo name
+});
